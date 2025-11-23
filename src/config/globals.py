a68fbcb826194f7f266429f28594902b1bf74dd7
Eduardo Bezerra <edubezerra@gmail.com>
2025-11-23 10:46:58 -0300

    Make globals configurable via environment variables

--- src/config/globals.py
+++ src/config/globals.py
@@ -1,35 +1,48 @@
-INMET_API_BASE_URL = "https://apitempo.inmet.gov.br"
+import os
+
+
+def _get_env(key: str, default: str) -> str:
+    return os.getenv(key, default)
+
+
+def _get_env_float(key: str, default: float) -> float:
+    try:
+        return float(os.getenv(key, default))
+    except (TypeError, ValueError):
+        return default
+
+INMET_API_BASE_URL = _get_env("INMET_API_BASE_URL", "https://apitempo.inmet.gov.br")
 
 # Weather stations datasource directories
-WS_INMET_DATA_DIR = "./data/ws/inmet/"
-WS_ALERTARIO_DATA_DIR = "./data/ws/alertario/ws/"
-GS_ALERTARIO_DATA_DIR = "./data/ws/alertario/rain_gauge_era5_fused/"
-CEMADEN_DATA_DIR = ".data/ws/cemaden/"
+WS_INMET_DATA_DIR = _get_env("WS_INMET_DATA_DIR", "./data/ws/inmet/")
+WS_ALERTARIO_DATA_DIR = _get_env("WS_ALERTARIO_DATA_DIR", "./data/ws/alertario/ws/")
+GS_ALERTARIO_DATA_DIR = _get_env("GS_ALERTARIO_DATA_DIR", "./data/ws/alertario/rain_gauge_era5_fused/")
+CEMADEN_DATA_DIR = _get_env("CEMADEN_DATA_DIR", ".data/ws/cemaden/")
 
-SUMARE_DATA_DIR = ".data/radar_sumare"
+SUMARE_DATA_DIR = _get_env("SUMARE_DATA_DIR", ".data/radar_sumare")
 # WS_GOES_DATA_DIR = "atmoseer/data/ws/goes16"
-GOES16_DATA_DIR = "./data/goes16/CMI/"
+GOES16_DATA_DIR = _get_env("GOES16_DATA_DIR", "./data/goes16/CMI/")
 
 # Directory to store the extracted features from GOES-16 data
-GOES16_FEATURES_DIR = "./data/goes16/features/"
+GOES16_FEATURES_DIR = _get_env("GOES16_FEATURES_DIR", "./data/goes16/features/")
 
 # Atmospheric sounding datasource directory
-NWP_DATA_DIR = "./data/NWP/"
+NWP_DATA_DIR = _get_env("NWP_DATA_DIR", "./data/NWP/")
 
 # Atmospheric sounding datasource directory
-AS_DATA_DIR = "./data/as/"
+AS_DATA_DIR = _get_env("AS_DATA_DIR", "./data/as/")
 
 # GOES16/DSI directory
-DSI_DATA_DIR = "./data/goes16/DSI"
+DSI_DATA_DIR = _get_env("DSI_DATA_DIR", "./data/goes16/DSI")
 
 # GOES16/TPW directory
-TPW_DATA_DIR = "./data/goes16/wsoi"
+TPW_DATA_DIR = _get_env("TPW_DATA_DIR", "./data/goes16/wsoi")
 
 # Directory to store the train/val/test datasets for each weather station of interest
-DATASETS_DIR = "./data/datasets/"
+DATASETS_DIR = _get_env("DATASETS_DIR", "./data/datasets/")
 
 # Directory to store the generated models and their corresponding reports
-MODELS_DIR = "./models/"
+MODELS_DIR = _get_env("MODELS_DIR", "./models/")
 
 # see https://portal.inmet.gov.br/paginas/catalogoaut
 INMET_WEATHER_STATION_IDS = (
@@ -80,7 +93,7 @@ ALERTARIO_WEATHER_STATION_IDS = (
     "sao_cristovao",  # **
 )
 
-WEBSIRENE_DATA_DIR = "./data/ws/websirene/"
+WEBSIRENE_DATA_DIR = _get_env("WEBSIRENE_DATA_DIR", "./data/ws/websirene/")
 
 WEBSIRENE_STATION_IDS = (
     "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
@@ -95,10 +108,13 @@ WEBSIRENE_STATION_IDS = (
 )
 
 # lower left corner
-lat_min, lon_min = (-23.801876626302175, -45.05290312102409)
+lat_min = _get_env_float("REGION_LAT_MIN", -23.801876626302175)
+lon_min = _get_env_float("REGION_LON_MIN", -45.05290312102409)
 
 # upper right corner
-lat_max, lon_max = (-21.699774257353113, -42.35676996062447)
+lat_max = _get_env_float("REGION_LAT_MAX", -21.699774257353113)
+lon_max = _get_env_float("REGION_LON_MAX", -42.35676996062447)
 
 # Region of Interest
 extent = [lon_min, lat_min, lon_max, lat_max]
+
